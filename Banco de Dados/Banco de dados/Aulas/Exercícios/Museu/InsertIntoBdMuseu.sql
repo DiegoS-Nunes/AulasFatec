b050945d71e884a7d0d16f11b1b33e1469f64a7b
Diego Nunes <diego.nunes@fatec.sp.gov.br>
2024-06-05 22:25:30 -0300

    Arrumando pastas e incluindo aula de junções

---
+++ Banco de Dados/Banco de dados/Aulas/Exercícios/Museu/InsertIntoBdMuseu.sql
@@ -0,0 +1,27 @@
+--INSERE LINHAS EM PAIS
+insert into PAIS values (1,'Brasil'),
+(2, 'Bélgica'),
+(3, 'Argentina'),
+(4, 'Espanha'),
+(5, 'Austrália')
+
+--INSERE LINHAS EM MUSEU
+insert into MUSEU values (1, 'Museu de artes de São Paulo',1),
+insert into MUSEU values (2, 'Museu de Groening',2),
+(3, 'Museu do Prado',4),
+(4, 'Museu de MONA',5)
+
+SET DATEFORMAT DMY
+
+INSERT INTO AUTOR VALUES (1, 'Henry Van de Velde', 2, '03/04/1863'),
+(2, 'Romero Brito', 1, '06/10/1963'),
+INSERT INTO AUTOR VALUES (3, 'Henry Van de Velde', 2, '03/04/1863')
+
+INSERT INTO ESCOLA VALUES (1,'Romantismo'),
+(2,'Cubismo'),
+(3,'Modernismo'),
+(4,'Expressionismo')
+
+INSERT INTO AUTORIA (COD_AUTORIA, COD_AUTOr, COD_ESCOLA, DATA_INICIO_ESCOLA) VALUES (1, 1, 2, '03/04/1893')
+
+INSERT INTO OBRA VALUES (1, 'Casa', 2, 1, 1)
